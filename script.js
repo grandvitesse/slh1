44cbaff3927395f781d12f8168b70565a834d91b
grandvitesse <167726010+grandvitesse@users.noreply.github.com>
2024-04-22 10:08:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ document.getElementById("passwordForm").addEventListener("submit", function(even
   event.preventDefault(); // Prevent the form from submitting
 
   var password = document.getElementById("password").value;
-
-  // Check if the password is correct
-  if (password === "saleha1") {
-    document.getElementById("letter").style.display = "block"; // Show the letter
+ // Check if the password is correct
+        if (password === "saleha1") {
+            // Open a new page with the letter content
+            var letterContent = document.getElementById("letter").innerHTML;
+            var newWindow = window.open("", "_blank");
+            newWindow.document.write("<!DOCTYPE html><html><head><title>Letter</title></head><body>" + letterContent + "</body></html>");
+ // // Check if the password is correct
+ // if (password === "saleha1") {
+ //   document.getElementById("letter").style.display = "block"; // Show the letter
   } else {
     alert("Incorrect password. Please try again."); // Show an alert for incorrect password
   }
